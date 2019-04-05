64b98dabc874f495dc4848551db3eb5c25872080
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-05 10:01:37 -0700

    Sprint MVP

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const db = require('../database/dbConfig.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = require('../auth/authenticate').jwtKey;
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,13 +12,44 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+// POST http://localhost:3300/api/register
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 4);
+  user.password = hash;
+
+  db('users')
+    .insert(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error registering', error: error });
+    });
 }
 
+// POST http://localhost:3300/api/login
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  db('users')
+    .where({username})
+    // .select('id', 'username', 'password')
+    .first()
+    .then(u => {
+      if (u && bcrypt.compareSync(password, u.password)) {
+        const token = makeToken(u);
+        res.status(200).json({ message: `Hello, ${u.username}!`, token });
+      } else {
+        res.status(401).json({ message: 'WRONG!' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error logging in', error: err });
+    });
 }
+
 
 function getJokes(req, res) {
   const requestOptions = {
@@ -29,4 +64,16 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '2d'
+  };
+
+  return jwt.sign(payload, secret, options);
 }
